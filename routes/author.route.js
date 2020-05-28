e1e1e95cda29511cab7e11d9919be9e987538272
Joel <joelmolina2121@gmail.com>
2020-05-27 22:02:54 -0300

    Corregi una falla en la funcionalidad de los delete

--- routes/author.route.js
+++ routes/author.route.js
@@ -1,81 +1,91 @@
 /**Definimos una ruta */
-const {Router} = require('express');
+const { Router } = require("express");
 const router = Router();
 
-/**Importamos lodash 
+/**Importamos lodash
  * Esta libreria sirve para manipular arrays
-*/
-const _ = require('lodash');
+ */
+const _ = require("lodash");
 
 /**Importamos los authors desde el json */
-const authors = require('../sample-author.json');
+const authors = require("../sample-author.json");
 
 /**Importamos los books desde el json */
-const books = require('../sample-book.json');
+const books = require("../sample-book.json");
 
 /**Definimos una ruta para recibir informacion
  * Con req obtenemos los datos que envia el usuario
  * Con res seteamos los que vamos a devolver
  */
 
-router.get('/authors',(req,res) =>{
-    res.json(authors);
+router.get("/authors", (req, res) => {
+  res.json(authors);
 });
 
-router.post('/authors',(req,res) =>{
-    const{name,lastname} = req.body;
-    if(name && lastname){
-        const newAuthor = {...req.body};
-        authors.push(newAuthor);
-        res.json({"added":"ok"});
-    }
-    else{
-        res.status(400).json({"statusCode":"Bad request"});
-    }
+router.post("/authors", (req, res) => {
+  const { name, lastname } = req.body;
+  if (name && lastname) {
+    const newAuthor = { ...req.body };
+    authors.push(newAuthor);
+    res.json({ added: "ok" });
+  } else {
+    res.status(400).json({ statusCode: "Bad request" });
+  }
 });
 
-router.put('/authors/:id',(req, res) =>{
-    const id = req.params.id;
-    const {name, lastname} = req.body;
-    /**Chequeo si el autor que quiero modificar esta agregado */
-    if ( _.find(authors, function(o) { return o.id == id; })){
-        if(name && lastname){
-            _.each(authors,(a) =>{
-                if (a.id == id){
-                    a.name = name;
-                    a.lastname = lastname;
-                }
-            });
-            res.json({"modified":"ok"});
+router.put("/authors/:id", (req, res) => {
+  const id = req.params.id;
+  const { name, lastname } = req.body;
+  /**Chequeo si el autor que quiero modificar esta agregado */
+  var existAuthor = _.find(authors, function (o) {
+    return o.id == id;
+  });
+  if (existAuthor) {
+    if (name && lastname) {
+      _.each(authors, (a) => {
+        if (a.id == id) {
+          a.name = name;
+          a.lastname = lastname;
         }
-        else{
-            res.status(400).json({"statusCode":"Bad request"});
-        }
+      });
+      res.json({ modified: "ok" });
+    } else {
+      res.status(400).json({ statusCode: "Bad request" });
     }
-    else {/**Devuelvo un 404 porque el autor que quiero modificar no existe */
-        res.status(404).json({"statusCode":"No hay un autor registrado que se corresponda con esa id"});
-    }
-    
+  } else {
+    /**Devuelvo un 404 porque el autor que quiero modificar no existe */
+    res.status(404).json({
+      statusCode: "No hay un autor registrado que se corresponda con esa id",
+    });
+  }
 });
 
-router.delete('/authors/:id',(req, res)=>{
-    const id = req.params.id;
-    /**Chequea si el autor que quiero borrar esta agregado */
-    if ( _.find(authors, function(o) { return o.id == id; })){
-        for (var i = 0 ; i < books.length ; i++){
-            /**Si el autor que quiero borrar tiene un libro entonces no lo puedo borrar */
-            if (books[i].authorId == id){
-                res.status(400).json({"statusCode":"Bad request"});
-            }
-        }
-        _.remove(authors,(author)=>{
-            return author.id == id;
-        })
-        res.json(authors);
+router.delete("/authors/:id", (req, res) => {
+  const id = req.params.id;
+  /**Chequea si el autor que quiero borrar esta agregado */
+  var existAuthor = _.find(authors, function (o) {
+    return o.id === id;
+  });
+  if (existAuthor) {
+    var haveBook = books.find((book) => book.authorId === id);
+    /**Si el autor que quiero borrar tiene un libro entonces no lo puedo borrar */
+    if (haveBook) {
+      res.status(400).json({
+        statusCode:
+          "No se puede borrar porque este autor tiene libros asociados",
+      });
+    } else {
+      _.remove(authors, (author) => {
+        return author.id == id;
+      });
+      res.json(authors);
     }
-    else {/**Devuelvo un 404 si el autor que quiero borrar no existe */
-        res.status(404).json({"statusCode":"No hay un autor registrado que se corresponda con esa id"});
-    }
-})
+  } else {
+    /**Devuelvo un 404 si el autor que quiero borrar no existe */
+    res.status(404).json({
+      statusCode: "No hay un autor registrado que se corresponda con esa id",
+    });
+  }
+});
 
 module.exports = router;
--- routes/book.route.js
+++ routes/book.route.js
@@ -1,89 +1,98 @@
 /**Definimos una ruta */
-const {Router} = require('express');
+const { Router } = require("express");
 const router = Router();
 
 /**Importamos lodash. Esta libreria sirve para manipular arrays */
-const _ = require('lodash');
+const _ = require("lodash");
 
 /**Importamos los books desde el json */
-const books = require('../sample-book.json');
+const books = require("../sample-book.json");
 
 /**Importamos los authors desde el json */
-const authors = require('../sample-author.json');
+const authors = require("../sample-author.json");
 
-router.get('/books',(req,res) =>{
-    /**Creo un array para almacenar los datos del libro y de su respectivo autor */
-    var array = new Array ();
-    for(var i = 0 ; i < books.length ; i++){
-        for (var j = 0 ; j < authors.length ; j++){
-            if (books[i].authorId == authors[j].id){
-                array[i] = {
-                    "id": books[i].id,
-                    "name": books[i].name,
-                    "authorId": books[i].authorId,
-                    "authorName" : authors[j].name,
-                    "authorLastname": authors[j].lastname
-                }
-            }
-        }
+router.get("/books", (req, res) => {
+  /**Creo un array para almacenar los datos del libro y de su respectivo autor */
+  var array = new Array();
+  for (var i = 0; i < books.length; i++) {
+    for (var j = 0; j < authors.length; j++) {
+      if (books[i].authorId == authors[j].id) {
+        array[i] = {
+          id: books[i].id,
+          name: books[i].name,
+          authorId: books[i].authorId,
+          authorName: authors[j].name,
+          authorLastname: authors[j].lastname,
+        };
+      }
     }
-    res.json(array);
+  }
+  res.json(array);
 });
 
-router.post('/books',(req,res) =>{
-    const{name,authorId} = req.body;
-    for (var i = 0 ; i < authors.length ; i++){
-        /**Chequea si hay un autor ingresado que tenga id igual a authorId */
-        if (authors[i].id == authorId){
-            if (name && authorId){
-                const newBook = {...req.body};
-                books.push(newBook);
-                res.json({"added":"ok"});
-            }
-            else{/**Si faltan datos entonces tiro un 400 */
-                res.status(400).json({"statusCode":"Bad request"});
-            }
-        }
+router.post("/books", (req, res) => {
+  const { name, authorId } = req.body;
+  var existAuthor = authors.find((author) => author.id === authorId);
+  /**Chequea si hay un autor ingresado que tenga id igual a authorId */
+  if (existAuthor) {
+    if (name && authorId) {
+      const newBook = { ...req.body };
+      books.push(newBook);
+      res.json({ added: "ok" });
+    } else {
+      /**Si faltan datos entonces tiro un 400 */
+      res.status(400).json({ statusCode: "Bad request" });
     }
-    /**Tiro un 404 porque quiero agregar un libro de un autor que no tengo agregado */
-    res.status(404).json({"statusCode":"No hay un autor ingresado que se corresponda con authorId"});
-    
+  }
+  /**Tiro un 404 porque quiero agregar un libro de un autor que no tengo agregado */
+  res.status(404).json({
+    statusCode: "No hay un autor ingresado que se corresponda con authorId",
+  });
 });
 
-router.put('/books/:id',(req, res) =>{
-    const id = req.params.id;
-    const {name, authorId} = req.body;
-    /**Chequea si el libro que quiero modificar esta agregado */
-    if ( _.find(books, function(o) { return o.id == id; })){
-        if (name && authorId){
-            _.each(books,(book) =>{
-                if (book.id == id){
-                    book.name = name;
-                    book.authorId = authorId;
-                }
-            });
-            res.json({"modified":"ok"});
+router.put("/books/:id", (req, res) => {
+  const id = req.params.id;
+  const { name, authorId } = req.body;
+  /**Chequea si el libro que quiero modificar esta agregado */
+  var existBook = _.find(books, function (o) {
+    return o.id === id;
+  });
+  if (existBook) {
+    if (name && authorId) {
+      _.each(books, (book) => {
+        if (book.id == id) {
+          book.name = name;
+          book.authorId = authorId;
         }
-        else{
-            res.status(400).json({"statusCode":"Bad request"});
-        }
+      });
+      res.json({ modified: "ok" });
+    } else {
+      res.status(400).json({ statusCode: "Bad request" });
     }
-    else {/**Tiro un 404 si intento modificar un libro que no tengo */
-        res.status(404).json({"statusCode":"No hay un libro registrado que se corresponda con ese id"}); 
-    }
-    
+  } else {
+    /**Tiro un 404 si intento modificar un libro que no tengo */
+    res.status(404).json({
+      statusCode: "No hay un libro registrado que se corresponda con ese id",
+    });
+  }
 });
 
-router.delete('/books/:id',(req, res)=>{
-    const id = req.params.id;
-    /**Chequea si el id del libro que quiero borrar existe */
-    if ( _.find(books, function(o) { return o.id == id; })){
-        _.remove(books,(book)=>{
-            return book.id == id
-        })
-        res.json(books);
-    }
-    res.status(404).json({"statusCode":"No hay un libro registrado que se corresponda con ese id"}); 
+router.delete("/books/:id", (req, res) => {
+  const id = req.params.id;
+  /**Chequea si el id del libro que quiero borrar existe */
+  var existBook = _.find(books, function (o) {
+    return o.id == id;
+  });
+  if (existBook) {
+    _.remove(books, (book) => {
+      return book.id == id;
+    });
+    res.json(books);
+  } else {
+    res.status(404).json({
+      statusCode: "No hay un libro registrado que se corresponda con ese id",
+    });
+  }
 });
 
 module.exports = router;
